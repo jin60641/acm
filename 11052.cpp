d858bc18215bb0b346ade3430efbfda3b3d6cb17
jin60641 <jin60641@naver.com>
2017-05-12 21:54:25 +0900

    dp2

--- 11052.cpp
+++ 11052.cpp
@@ -1,35 +1,27 @@
 #include<stdio.h>
-#include<map>
-#include<algorithm>
-using namespace std;
+int N,K,a[1000],result[1001];
+
+int f( int cnt, int cost ){
+	if( result[cnt] > cost ){
+		return -1;
+	} else {
+		result[cnt] = cost;
+	}
+	int max = result[cnt];
+	for( int i = 0; i < N-cnt; ++i ){
+		int c = f( cnt + (i+1), cost + a[i] );
+		if( c > max ){
+			max = c;
+		}
+	}
+	return max;
+}
 
 int main(){
-	int N;
-	map<float, int> m;
-	scanf("%d",&N);
-	float P[1000];
-	for( int i = 0; i < N; ++i ){
-		float f;
-		scanf("%f",&f);
-		P[i] = f/(i+1);
-		if( m[P[i]] > 0 ){
-			continue;
-		}
-		m[P[i]] = i+1;
-	}
-	sort(P,P+N);
-	int cnt = N;
-	int total = 0;
-	for( int i = N-1; i >= 0; --i ) {
-		if( cnt >= m[P[i]] ){
-			int n = cnt/m[P[i]];
-			cnt -= m[P[i]]*n;
-			total += P[i]*m[P[i]]*n;
-		}
-		if( cnt == 0 ){
-			break;
-		}
-	}
-	printf("%d\n",total);
-	return 0;
+    scanf("%d",&N);
+    for( int i = 0; i < N; ++i ){
+        scanf("%d",&a[i]);
+    }
+	int r = f(0,0);
+	printf("%d\n",r);
 }
