397824182b07fc145f7237e7b24d98132abcac8b
jin60641 <jin60641@naver.com>
2017-05-13 17:01:35 +0900

    11052 clear

--- 11052.cpp
+++ 11052.cpp
@@ -1,27 +1,24 @@
 #include<stdio.h>
-int N,K,a[1000],result[1001];
 
-int f( int cnt, int cost ){
-	if( result[cnt] > cost ){
-		return -1;
-	} else {
-		result[cnt] = cost;
-	}
-	int max = result[cnt];
-	for( int i = 0; i < N-cnt; ++i ){
-		int c = f( cnt + (i+1), cost + a[i] );
-		if( c > max ){
-			max = c;
-		}
-	}
-	return max;
+int N;
+int arr[1001];
+int result[1001];
+
+int max(int a, int b){
+	return a>b?a:b;
 }
 
 int main(){
-    scanf("%d",&N);
-    for( int i = 0; i < N; ++i ){
-        scanf("%d",&a[i]);
-    }
-	int r = f(0,0);
-	printf("%d\n",r);
+	scanf("%d",&N);
+	for( int i = 1; i <= N; ++i ){
+		scanf("%d",&arr[i]);
+	}
+	for(int i = 1; i <= N; ++i ){
+		int r = 0;
+		for( int j = 1; j <= i; ++j ){
+			r = max(r,result[i-j]+arr[j]);
+		}
+		result[i] = r;
+	}
+	printf("%d\n",result[N]);
 }
