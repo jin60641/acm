04ca79d02cf3cb8fe6e3c8495d694b5511a5f938
jin60641 <jin60641@naver.com>
2017-05-15 20:21:51 +0900

    7579&2624 clear

--- 7579.cpp
+++ 7579.cpp
@@ -1,7 +1,3 @@
-#include<stdio.h>
-int N,M,m[101],c[101];
-long long int dp[20000001];
-
 /*
 	N : 실행중인 앱의 갯수
 	M : 확보해야 하는 메모리
@@ -10,14 +6,16 @@ long long int dp[20000001];
 	dp : i만큼의 메모리를 비활성화 하는데 필요한 메모리
 */
 
-long long int min( long long int a, long long int b ){
-	if( a == -1 ){
-		return b;
-	} else if( b == -1 ){
-		return a;
-	} else {
-		return a<b?a:b;
-	}
+#include<stdio.h>
+int N,M,m[101],c[101];
+int dp[10001];
+
+int max( int a, int b ){
+	return a>b?a:b;
+}
+
+int min( int a, int b ){
+	return a<b?a:b;
 }
 
 int main(){
@@ -29,20 +27,19 @@ int main(){
         scanf("%d",&c[i]);
     }
 	dp[0] = 0;
-	for( int i = 1; i <= 20000000; ++i ){
+	for( int i = 1; i <= 10000; ++i ){
 		dp[i] = -1;
 	}
-	long long int result = 1010000001;
+	int result = 10002;
 	for( int i = 1; i <= N; ++i ){
-		for( int j = M+10000000; j >= 1; --j ){
-			if( m[i] <= j && ( dp[j-m[i]] >= 0 ) ){
-				dp[j] = min(dp[j], dp[j-m[i]] + c[i]);
-				if( j >= M ){
-					result = min( result, dp[j] );
+		for( int j = 10000; j >= 0; --j ){
+			if( dp[j] != -1 ){
+				dp[j+c[i]] = max(dp[j+c[i]],dp[j]+m[i]);
+				if( dp[j+c[i]] >= M ){
+					result = min(j+c[i],result);
 				}
 			}
 		}
 	}
-	
-	printf("%lld",result);
+	printf("%d\n",result);
 }
