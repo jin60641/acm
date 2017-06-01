cb9dff8006f543a19000c086bda0c8a97697deef
jin60641 <jin60641@naver.com>
2017-06-01 20:27:07 +0900

    6603 clear

--- 6603.cpp
+++ 6603.cpp
@@ -1,6 +1,14 @@
 #include<stdio.h>
 
+int s[12];
+int check[2000][6];
+int K = 0;
+int max;
+
 int c(int l, int r){
+	if( l < 0 || r < 0 || l < r){
+		return 0;
+	}
 	if( l-r < r ){
 		r = l-r;
 	}
@@ -8,24 +16,51 @@ int c(int l, int r){
 	for( int i = 0; i < r; ++i ){
 		result = result*(l-i)/(r-i);
 	}
-	return (int)result;
+	return (int)(result+1e-9);
+}
+
+int fill( int start, int weight, int index ){
+	int cnt = 0;
+	for( int i = start; i <= K-(6-weight); ++i ){
+		int tmp = cnt;
+		int l = K-i-1;
+		int r = 6-weight-1;
+		int a = c(l,r);
+		if( a == 0 ){
+			continue;
+		}
+		cnt += a;
+		for( int j = tmp; j < cnt; ++j ){
+			check[j+index][weight] = s[i];
+		}
+		fill(i+1,weight+1,tmp+index);
+	}
+	return 0;
 }
 
 int main(){
-	int k;
 	while(true){
-		scanf("%d",&k);
-		if( k == 0 ){
+		for( int j = c(K,6)-1; j >= 0; --j ){
+			for( int i = 0; i < 6; ++i ){
+				check[j][i] = 0;
+			}
+		}
+		scanf("%d",&K);
+		max = c(K,6);
+		if( K == 0 ){
 			return 0;
 		}
-		int s[12];
-		int check[2000][6];
-		for( int i = 0; i < k; ++i ){
+		for( int i = 0; i < K; ++i ){
 			scanf("%d",&s[i]);
 		}
-		for( int i = 0; i <= k-6; ++i ){
-			printf("%d\n",c(k-i-1,5));
+		fill(0,0,0);
+		for( int i = 0; i < max; ++i ){
+			for( int j = 0; j < 6; ++j ){
+				printf("%d ",check[i][j]);
+			}
+			printf("\n");
 		}
+		printf("\n");
 	}
 	return 0;
 }
