34c31160c947021e106239851cf32a2a2905c82c
jin60641 <jin60641@naver.com>
2017-07-09 00:14:22 +0900

    2841 clear, 2343 solving

--- 2841.cpp
+++ 2841.cpp
@@ -1,21 +1,35 @@
 #include<stdio.h>
-int check[500000];
+#include<vector>
+#include<algorithm>
+
+using namespace std;
+
+vector<int> check[6];
+
 int main(){
 	int N,P;
 	scanf("%d %d",&N,&P);
-	int cnt = 0;
+	long long int tot = 0;
+	for( int i = 0; i < 6; ++i ){
+		vector<int> v;
+		v.push_back(0);
+		check[i] = v;
+	}
 	for( int i = 0; i < N; ++i ){
 		int n,p;
-		scanf("%d",&n,&p);
-		if(	check[n] == 0 ){
-			check[n] = p;
-			++cnt;
-		} else if( check[n] < p ){
-			check[n] = p;
-			++cnt;
-		} else if( check[n] > p ){
-			check[n] = 2;
-			cnt+=2;
+		scanf("%d %d",&n,&p);
+		--n;
+		if( p < check[n].back() && ( find(check[n].begin(),check[n].end(),p) == check[n].end() ) ){
+			while( p < check[n].back() ){
+				check[n].pop_back();
+				++tot;
+			}
 		}
+		if( p > check[n].back() ){
+			check[n].push_back(p);
+			++tot;
+		}
+	printf("%lld\n",tot);
 	}
+	printf("%lld\n",tot);
 }
