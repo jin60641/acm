9cde8147bcf2acc9be3c541c66ce40ddb3d7c588
jin60641 <jin60641@naver.com>
2017-07-12 14:15:17 +0900

    2981 clear

--- 2981.cpp
+++ 2981.cpp
@@ -1,5 +1,6 @@
 #include<stdio.h>
 #include<algorithm>
+#include<stack>
 
 int f(int a,int b){
 	if( a < b ){
@@ -7,7 +8,6 @@ int f(int a,int b){
 		b = a;
 		a = tmp;
 	}
-	printf("%d %d\n",a,b);
 	while( b > 0 ){
 		int r = a%b;
 		a = b;
@@ -16,7 +16,7 @@ int f(int a,int b){
 	return a;
 }
 
-int result[1000000];
+int small[1000000];
 
 int main(){
 	int N;
@@ -25,35 +25,28 @@ int main(){
 	for( int i = 0; i < N; ++i ){
 		scanf("%d",&a[i]);
 	}
+	int tmp = 0;
+	for( int i = 1; i < N; ++i ){
+		tmp = f(tmp,a[i]-a[i-1]);
+	}
 	int index = 0;
-	int flag = 0;
-	while( flag == 0 ){
-		int tmp = a[0];
-		for( int i = 1; i < N; ++i ){
-			tmp = f(tmp,a[i]);
+	std::stack<int> big;
+	for( int i = 1; i*i <= tmp; ++i ){
+		if( tmp%i == 0 ){
+			small[index++] = i;
+			big.push(tmp/i);
 		}
-		//if( tmp > 1 ){
-			result[index++] = tmp;
-		//}
-		for( int i = 0; i < N; ++i ){
-			a[i] -= tmp;
-			if( a[i] < 0 ){
-				flag = 1;
-				break;
-			}
-		}
-	}
-	std::sort(result,result+index);
-	for( int i = 0; i < index; ++i ){
-		printf("%d ",result[i]);
-	}
-	printf("\n");
-	if( result[0] != 1 ){
-		printf("%d ",result[0]);
 	}
 	for( int i = 1; i < index; ++i ){
-		if( result[i] != result[i-1] && result[i] != 0 ){
-			printf("%d ",result[i]);
-		}
+		printf("%d ",small[i]);
 	}
+	--index;
+	while( big.size() > 0 ){
+		if( small[index] != big.top() ){
+			printf("%d ",big.top() );
+		}
+		big.pop();
+	}
+
+	return 0;
 }
