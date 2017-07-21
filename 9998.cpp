247ae1eea858901b4a1febd8c0d47324f4eb39b0
jin60641 <jin60641@naver.com>
2017-07-21 18:22:51 +0900

    ps

---
+++ 9998.cpp
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main(){
+	int min = 1000000;
+	int N;
+	int a[300000];
+	int b[300000];
+	scanf("%d",N);
+	for( int i = 1; i <= N; ++i ){
+		scanf("%d",&a[i]);
+	}
+	for( int i = 1; i <= N; ++i ){
+		scanf("%d",&b[i]);
+	}
+	
+}
