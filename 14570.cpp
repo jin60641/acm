2af54c4f3e6e1d238e1668c33b94817b8fbaf79a
jin60641 <jin60641@naver.com>
2017-08-02 06:37:44 +0900

    14570(binary-tree) solved, 3780(union-find) not-solved

--- 14570.cpp
+++ 14570.cpp
@@ -1,35 +1,38 @@
 #include<stdio.h>
-#include<vector>
-
-std::vector<int> leaf[2];
-
 struct node {
-	int parent,left,right;
+    int left,right;
 };
 
 node n[200001];
 
-int main(){
-	int N;
-	scanf("%d",&N);
-	for( int i = 1; i <= N; ++i ){
-		scanf("%d %d",&n[i].left,&n[i].right);
-		if( n[i].left != -1 ){
-			n[n[i].left].parent = i;
-		}
-		if( n[i].right != -1 ){
-			n[n[i].right].parent = i;
-		}
-	}
-	
-	//좌측 리프노드탐색
-	//우측 리프노드탐색
-	
-	int K;
-	scanf("%d",&K);
-	//K를 2로 나눈 나머지가 0이면 좌측 리프노드의 사이클에 포함됨
-	//K를 2로 나눈 나머지가 1이면 우측 리프노드의 사이클에 포함됨
-	//K를 2로 나눈 몫을 사이클의 길이로 나눈 나머지가 n이라면 사이클[n]이 답이됨
-	
-	return 0;
+int find( int now, long long int K ){
+    if( n[now].right == -1 && n[now].left == -1 ){
+        return now;
+    } else if( n[now].right == -1 ){
+        return find( n[now].left, K );
+    } else if( n[now].left == -1 ){
+        return find( n[now].right, K );
+    }
+    if( K%2LL == 0LL ){
+        return find( n[now].right, K/2LL );
+    } else {
+        return find( n[now].left, K/2LL + 1LL );
+    }
 }
+
+int main(){
+    int N;
+    scanf("%d",&N);
+    for( int i = 1; i <= N; ++i ){
+        int l,r;
+        scanf("%d %d",&l,&r);
+        n[i].left = l;
+        n[i].right = r;
+    }
+
+    long long int K;
+    scanf("%lld",&K);
+	printf("%d\n",find(1,K));
+    return 0;
+}
+
