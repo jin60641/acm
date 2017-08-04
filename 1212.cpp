2e12711643accb5ab8e9a27536c5437bd4780082
jin60641 <jin60641@naver.com>
2017-08-05 08:16:30 +0900

    easy ps

--- 1212.cpp
+++ 1212.cpp
@@ -1,19 +1,32 @@
 #include<stdio.h>
+#include<string.h>
+#include<stack>
 
 int main(){
-	char s[333335];
-	scanf("%s",s);
-	int length = 0;
-	for( ; s[length] != '\0'; ++length );
-	long long int pos = 1;
-	long long int tot = 0;
-	for( int i = length-1; i >= 0; --i ){
-		int a = s[i]-48;
-		tot += pos * a;
-		pos *= 8;
+	char o[333335];
+	std::stack<int> s;
+	scanf("%s",o);
+	int n = strlen(o);
+	if( n == 1 && o[0] == '0' ){
+		printf("0\n");
+		return 0;
 	}
-	int 
-	for( int i = 0; tot>0; ++i )[
-		
+	for( int i = n-1; i >= 0; --i ){
+		int a = o[i] - '0';
+		for( int j = 0; j < 3; ++j ){
+			s.push(a%2);
+			a/=2;
+		}
 	}
+	int flag = 0;
+	while( s.size() > 0 ){
+		if( flag == 0 && s.top() == 0 ){
+		} else {
+			flag = 1;
+			printf("%d",s.top());
+		}
+		s.pop();
+	}
+	printf("\n");
+	return 0;
 }
---
+++ 1629.cpp
@@ -0,0 +1,9 @@
+#include<stdio.h>
+
+int main(){
+	long long int A,B,C;
+	scanf("%lld %lld %lld",&A,&B,&C);
+	A = A%C;
+	for( int i = 0; i < B;
+	return 0;
+}
