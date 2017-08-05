8ff1237765ac948db8e6f1ed2812ad12276551d5
jin60641 <jin60641@naver.com>
2017-08-06 02:02:08 +0900

    easy ps

--- 1629.cpp
+++ 1629.cpp
@@ -1,9 +1,22 @@
 #include<stdio.h>
+#include<queue>
 
 int main(){
 	long long int A,B,C;
 	scanf("%lld %lld %lld",&A,&B,&C);
-	A = A%C;
-	for( int i = 0; i < B;
+	std::queue<int> s;
+	while(B!=0){
+		s.push(B%2);
+		B/=2;
+	}
+	long long int tot = 1;
+	while( s.size() > 0 ){
+		if( s.front() == 1 ){
+			tot = (tot*(A))%C;
+		}
+		A = (A*A)%C;
+		s.pop();
+	}
+	printf("%lld\n",tot%C);
 	return 0;
 }
