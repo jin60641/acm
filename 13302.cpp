fa05be6ab5f79e55772d25223ef15b0ce59717f4
jin60641 <jin60641@naver.com>
2017-08-09 02:02:33 +0900

    13302 clear ( multi dimensional dp )

--- 13302.cpp
+++ 13302.cpp
@@ -13,15 +13,14 @@ struct pass {
 	int coupon;
 	int day;
 };
-
 const pass p[3] = { { 10000, 0, 1 }, { 25000, 1, 3 }, { 37000, 2, 5 } };
 int arr[101];
-int dp[101][25];
+int dp[101][55];
 int main(){
 	int N,M;
 	scanf("%d %d",&N,&M);
 	for( int i = 0; i <= N; ++i ){
-		for( int j = 0; j < 25; ++j ){
+		for( int j = 0; j < 50; ++j ){
 			dp[i][j] = -1;
 		}
 	}
@@ -33,28 +32,26 @@ int main(){
 	}
 	for( int i = 1; i <= N; ++i ){
 		if( arr[i] == 0 ){
-			for( int j = 0; j < 25; ++j ){
+			for( int j = 29; j >= 0; --j ){
 				if( dp[i-1][j] != -1 ){
 					for( int k = 0; k < 3; ++k ){
 						for( int m = 0; m < p[k].day; ++m ){
 							dp[i+m][j+p[k].coupon] = min( dp[i+m][j+p[k].coupon], dp[i-1][j] + p[k].price );
-						}
-						if( j >= 3 && k == 0 ){
-							for( int m = 1; m <= j/3; ++m ){
-								dp[i][j-m*3] = min( dp[i][j-m*3], ( dp[i][j] - (p[0].price*m) ) );
+							if( k == 0 && dp[i-1][j+3] != -1 ){ 
+								dp[i][j] = min( dp[i-1][j+3], dp[i][j] );
 							}
 						}
 					}
 				}
 			}
 		} else {
-			for( int j = 0; j < 3; ++j ){
+			for( int j = 0; j < 30; ++j ){
 				dp[i][j] = min( dp[i][j], dp[i-1][j] );
 			}
 		}
 	}
 	int result = -1; 
-	for( int i = 0; i < 25; ++i ){
+	for( int i = 0; i < 30; ++i ){
 		result = min( result, dp[N][i] );
 	}
 	printf("%d\n",result );
