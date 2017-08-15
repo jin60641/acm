6434ac20b2282ba9d940f36b09062d37a373f073
jin60641 <jin60641@naver.com>
2017-08-15 12:51:35 +0900

    easy ps

--- 5719.cpp
+++ 5719.cpp
@@ -1,68 +1,106 @@
 #include<stdio.h>
 #include<vector>
 #include<queue>
-#include<algorithm>
-const int INF = 500*1000+1;
+const int INF = 200000000;
+int N = 500,M,start,end;
 struct pair {
-	int to, w;
+	int n, w;
 };
-std::queue<int> q;
-std::vector<pair> v[500];
-std::vector<int> result;
+struct edge {
+	int to,w;
+};
+std::vector<edge> v[500];
+std::vector<int> path[500];
+struct cmp {
+	bool operator()(pair a, pair b){
+		return a.w < b.w;
+	}
+};
 int check[500];
-int start,end,N,M;
-int bfs(){
-	int now = q.front();
-	for( int i = 0; i < v[now].size(); ++i ){
-		if( v[now][i].to == end ){
-			result.push_back(check[now] + v[now][i].w);
-		} else if( check[v[now][i].to] > check[now] + v[now][i].w ){
-			check[v[now][i].to] = check[now] + v[now][i].w;
-			q.push(v[now][i].to);
+int shortest[500][500];
+std::priority_queue<pair,std::vector<pair>,cmp> pq;
+
+int dfs(int now){
+	for( int i = 0; i < path[now].size(); ++i ){
+		if( shortest[path[now][i]][now] == 0 ){
+			shortest[path[now][i]][now] = 1;
+			dfs(path[now][i]);
 		}
 	}
-	q.pop();
 	return 0;
 }
-int clear(){
+
+void clear(){
 	for( int i = 0; i < N; ++i ){
-		v[i].clear();
+		for( int j = 0; j < N; ++j ){
+			shortest[i][j] = 0;
+		}
 		check[i] = INF;
+		v[i].clear();
+		path[i].clear();
+	}
+}
+
+int bfs(){
+	pair now = pq.top();
+	pq.pop();
+	if( now.w > check[now.n] ){
+		return 0;
+	} else if( now.n == end ){
+		return 0;
+	}
+	for( int i = 0; i < v[now.n].size(); ++i ){
+		edge n = v[now.n][i];
+		if( shortest[now.n][n.to] == 1 ){
+			continue;
+		}
+		if( check[n.to] > now.w + n.w ){
+			check[n.to] = now.w + n.w;
+			pair p = { n.to, check[n.to] };
+			path[n.to].clear();
+			path[n.to].push_back(now.n);
+			pq.push(p);
+		} else if( check[n.to] == now.w + n.w ){
+			path[n.to].push_back(now.n);
+		}
 	}
 	return 0;
 }
 int main(){
 	while( true ){
+		clear();
 		scanf("%d %d",&N,&M);
 		if( N == 0 && M == 0 ){
-			break;
+			return 0;
 		}
-		clear();
 		scanf("%d %d",&start,&end);
 		for( int i = 0; i < M; ++i ){
-			int a,b,w;
-			scanf("%d %d %d",&a,&b,&w);
-			pair p = { b, w };
-			v[a].push_back(p);
+			int from;
+			edge e;
+			scanf("%d %d %d",&from,&e.to,&e.w);
+			v[from].push_back(e);
 		}
-		q.push(start);
 		check[start] = 0;
-		while( q.size() > 0 ){
+		pair s = { start, 0 };
+		pq.push(s);
+		while( pq.size() > 0 ){
 			bfs();
 		}
-		std::sort(result.begin(),result.end());
-		if( result.size() <= 1 ){
+		if( check[end] == INF ){
 			printf("-1\n");
 			continue;
 		}
-		int flag = -1;
-		for( int i = 1; i < result.size(); ++i ){
-			if( result[i] != result[i-1] ){
-				flag = result[i];
-				break;
-			}
+		for( int i = 0; i < N; ++i ){
+			check[i] = INF;
 		}
-		printf("%d\n",flag);
+		dfs(end);
+		check[start] = 0;
+		check[end] = INF;
+		pq.push(s);
+		while( pq.size() > 0 ){
+			bfs();
+		}
+		printf("%d\n",check[end]==INF?-1:check[end]);
 	}
-	return 0;
 }
+
