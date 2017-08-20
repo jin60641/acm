6539cd4cd3b1b35aae40a90e963e2c0e77aa610b
jin60641 <jin60641@naver.com>
2017-08-20 23:31:03 +0900

    Dijkstra study ( priority queue )

--- 11068.cpp
+++ 11068.cpp
@@ -1,4 +1,5 @@
 #include<stdio.h>
+#include<vector>
 
 int main(){
 	int N;
@@ -6,7 +7,26 @@ int main(){
 	while( N-- ){
 		int a;
 		scanf("%d",&a);
-		
+		int flag = 0;
+		for( int i = 2; i <= 64; ++i ){
+			int tmp = a;
+			std::vector<int> v;
+			while( tmp != 0 ){
+				v.push_back(tmp%i);
+				tmp/=i;
+			}
+			flag = 1;
+			for( int j = 0; j < v.size()/2; ++j ){
+				if( v[j] != v[v.size()-1-j] ){
+					flag = 0;
+					break;
+				}
+			}
+			if( flag == 1 ){
+				break;
+			}
+		}
+		printf("%d\n",flag);
 	}
 	return 0;
 }
