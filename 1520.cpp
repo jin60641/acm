dcdce59e0134f58dcb0e58f94c7a83b18401f570
jin60641 <jin60641@naver.com>
2017-10-30 21:06:06 +0900

    1520 clear ( topolgical sort, bfs, dp )

--- 1520.cpp
+++ 1520.cpp
@@ -1,27 +1,48 @@
 #include<stdio.h>
-int a[600][600];
+#include<vector>
+#include<queue>
 int Y,X,H;
+struct pos {
+	int y,x;
+};
+std::vector<pos> v[501][501];
+std::queue<pos> q;
+int a[501][501];
+int in[501][501];
+int cnt[501][501];
+int check[501][501];
 
-int f(int y,int x){
-	if( y == Y && x == X ){
-		++H;
-		return 0;
-	}
-	if( y < Y && ( a[y+1][x] < a[y][x] ) ){
-		f(y+1,x);
-	}
-	if( y > 1 && ( a[y-1][x] < a[y][x] ) ){
-		f(y-1,x);
-	}
-	if( x < X && ( a[y][x+1] < a[y][x] ) ){
-		f(y,x+1);
-	}
-	if( x > 1 && ( a[y][x-1] < a[y][x] ) ){
-		f(y,x-1);
-	}
-	return 0;
+void swap( int &a, int &b ){
+	int tmp = a;
+	a = b;
+	b = tmp;
 }
 
+void bfs(){
+	pos now = q.front();
+	pos d[4] = { { 1, 0 }, { -1, 0 }, { 0, 1 }, { 0, -1 } };
+	for( int i = 0; i < 4; ++i ){
+		int dy = now.y + d[i].y;
+		int dx = now.x + d[i].x;
+		if( ( dy != 1 || dx != 1 ) && a[dy][dx] < a[now.y][now.x] && check[dy][dx] == 0 && ( in[dy][dx] >= 1 && --in[dy][dx] == 0 ) ){
+			pos next = { dy, dx };
+			check[dy][dx] = 1;
+			q.push(next);
+		}
+	}
+}
+
+void f(){
+	pos now = q.front();
+	for( int i = v[now.y][now.x].size() - 1; i >= 0; --i ){
+		pos next = v[now.y][now.x][i];
+		int ny = next.y, nx = next.x;
+		cnt[ny][nx] += cnt[now.y][now.x];
+		if( in[ny][nx] >= 1 && --in[ny][nx] == 0 ){
+			q.push(next);
+		}
+	}
+}
 
 int main(){
 	scanf("%d %d",&Y,&X);
@@ -30,7 +51,44 @@ int main(){
 			scanf("%d",&a[i][j]);
 		}
 	}
-	f(1,1);
-	printf("%d\n",H);
+	pos d[2] = { { 0, 1 }, { 1, 0 } };
+	for( int i = 1; i <= Y; ++i ){
+		for( int j = 1; j <= X; ++j ){
+			for( int k = 0; k < 2; ++k ){
+				int ti = i, tj = j;
+				int di = i+d[k].y, dj = j+d[k].x;
+				if( a[di][dj] != 0 && a[i][j] != a[di][dj] ){
+					if( a[i][j] < a[di][dj] ){
+						swap(ti,di);
+						swap(tj,dj);
+					}
+					pos p = { di, dj };
+					v[ti][tj].push_back(p);
+					in[di][dj] += 1;
+				}
+			}
+		}
+	}
+	check[1][1] = 1;
+    for( int i = 1; i <= Y; ++i ){
+        for( int j = 1; j <= X; ++j ){
+			if( ( i != 1 || j != 1 ) && check[i][j] == 0 && in[i][j] == 0 ){
+				pos p = { i, j };
+				q.push(p);
+				while(q.size()>0){
+					bfs();
+					q.pop();
+				}
+			}
+        }
+    }
+	cnt[1][1] = 1;
+	pos start = { 1, 1 };
+	q.push(start);
+	while(q.size()>0){
+		f();
+		q.pop();
+	}
+	printf("%d\n",cnt[Y][X]);
 	return 0;
 }
