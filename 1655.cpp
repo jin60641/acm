f165a35951ea4c64f11282410fac2eb1631534a2
jin60641 <jin60641@naver.com>
2017-10-31 12:36:06 +0900

    1655 clear ( max&min heap )

--- 1655.cpp
+++ 1655.cpp
@@ -1,62 +1,56 @@
 #include<stdio.h>
-int size = 0;
-int h[100001];
-int abs( int a ){
-	return a<0?-a:a;
-}
-int min( int a, int b ){
-	if( a == -10001 ){
-		return b;
-	} else if( b == -10001 ){
-		return a;
-	} else {
-		return a<b?a:b;
+#include<queue>
+#include<vector>
+std::priority_queue<int> pq;
+
+struct cmp {
+	bool operator()(int a, int b){
+		return a > b;
 	}
-}
-int cal( int a ){
-	if( a <= 2 ){
-		printf("cal %d : %d\n",h[a],abs(h[a]-h[1])+abs(h[a]-h[2]));
-		return abs(h[a]-h[1])+abs(h[a]-h[2]);
-	} else {
-		printf("cal %d : %d\n",h[a],min( abs(h[a]-h[1])+abs(h[a]-h[0]), abs(h[a]-h[0])+abs(h[a]-h[2]) ));
-		return min( abs(h[a]-h[1])+abs(h[a]-h[0]), abs(h[a]-h[0])+abs(h[a]-h[2]) );
-	}
-}
-void swap(int a, int b){
-	int tmp = h[a];
-	h[a] = h[b];
-	h[b] = tmp;
-}
-void up( int a ){
-	if( a != 0 && cal(a) < cal((a-1)/2) ){
-		swap(a, (a-1)/2);
-		up((a-1)/2);
-	} else if( a >= 3 && a <= 6 && cal(a) == cal((a-1)/2) && cal(0) > cal(a) ){
-		swap(a, 0);
-	}
-}
-void push( int a ){
-	h[size] = a;
-	up(size++);
-}
-int top(){
-	return h[0];
-}
+};
+std::priority_queue<int,std::vector<int>,cmp> big;
+std::priority_queue<int> small;
+
+
+int N;
 int main(){
-	int N;
 	scanf("%d",&N);
-	for( int i = 0; i < N; ++i ){
-		h[i] = 10001;
-	}
 	for( int i = 0; i < N; ++i ){
 		int a;
 		scanf("%d",&a);
-		push(a);
-		for(int j = 0; j < size; ++j ){
-			printf("%d ",h[j]);
+		if( small.size() == 0 && big.size() == 0 ){
+			small.push(a);
+		} else if( small.size() != 0 && big.size() == 0 ){
+			if( small.top() > a ){
+				big.push( small.top() );
+				small.pop();
+				small.push(a);
+			} else {
+				big.push( a );
+			}
+		} else {
+			if( a <= small.top() ){
+				small.push(a);
+			} else if( small.top() <= a && a <= big.top() ){
+				small.push(a);
+			} else if( big.top() <= a ){
+				big.push(a);
+			}
 		}
-		printf("\n");
-		printf("%d\n",top());
+		if( small.size() > big.size() &&  small.size() - big.size() >= 2 ){
+			big.push( small.top() );
+			small.pop();
+		} else if( big.size() > small.size() && big.size() - small.size() >= 2 ){
+			small.push( big.top() );
+			big.pop();
+		}
+		if( big.size() == small.size() ){
+			printf("%d\n",small.top());
+		} else if( big.size() > small.size() ){
+			printf("%d\n",big.top());
+		} else if( small.size() > big.size() ){
+			printf("%d\n",small.top());
+		}
 	}
 	return 0;
 }
