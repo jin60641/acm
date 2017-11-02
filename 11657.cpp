f97e6e519963d64905f47a882da9bb6559d7fabb
jin60641 <jin60641@naver.com>
2017-11-02 23:46:23 +0900

    some ps

--- 11657.cpp
+++ 11657.cpp
@@ -1,5 +1,8 @@
 #include<stdio.h>
 const int INF = 500000001;
+int min( int a, int b ){
+	return a<b?a:b;
+}
 struct edge {
 	int from, to, weight;
 };
@@ -16,8 +19,8 @@ int main(){
 	}
 	for( int i = 1; i <= N-1; ++i ){
 		for( int j = 0; j < M; ++j ){
-			if( check[e[j].from] != INF && ( check[e[j].from] + e[j].weight < check[e[j].to] ) ){
-				check[e[j].to] = check[e[j].from] + e[j].weight;
+			if( check[e[j].from] != INF ){
+				check[e[j].to] = min( check[e[j].to], check[e[j].from] + e[j].weight );
 			}
 		}
 	}
