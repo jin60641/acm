c1010ab86e619d7e260b7fe47bbed6796017b56b
jin60641 <jin60641@naver.com>
2017-11-06 20:50:24 +0900

    2792 clear (parametric search)

--- 2792.cpp
+++ 2792.cpp
@@ -1,43 +1,27 @@
 #include<stdio.h>
 
-int max( int a, int b ){
+int max(int a, int b){
 	return a>b?a:b;
 }
 
-int min( int a, int b ){
-	return a<b?a:b;
-}
-
 int main(){
-	int N, M;
+	int N, M, m[300001], left = 1, right = 0;
 	scanf("%d %d",&N,&M);
-	int a[300000];
-	int left = 1;
-	int right = 0;
-	int result = 200000000;
 	for( int i = 0; i < M; ++i ){
-		scanf("%d",&a[i]);
-		right += a[i];
+		scanf("%d",&m[i]);
+		right = max(m[i],right);
 	}
 	while( left <= right ){
-		int mid = ( left + right ) / 2;
-		int cnt = 0;
-		int tmp = 0;
+		int mid = (left+right)/2, cnt = 0;
 		for( int i = 0; i < M; ++i ){
-			if( cnt + a[i]/mid ){
-				
-			}
-			tmp = max( tmp, mid + a[i]%mid );
-			cnt += a[i]/mid;
+			cnt += (m[i]%mid==0)?m[i]/mid:m[i]/mid+1;
 		}
-		printf("mid : %d / cnt : %d / tmp : %d\n",mid,cnt,tmp);
-		if( cnt >= N ){
+		if( cnt > N ){
 			left = mid+1;
-			result = min( result, tmp );
 		} else {
 			right = mid-1;
 		}
 	}
-	printf("%d",result);
+	printf("%d\n",left);
 	return 0;
 }
